348d4205d50deaf881a1469111d914b8a10defb8
Akshat <akshat.shah@mtap.com>
2022-11-07 16:12:58 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// import './App.css';
+
+// function App() {
+
+//   const inputArea = document.querySelector(".large-area--input");
+//   const outputArea = document.querySelector(".large-area--output");
+//   const btnFormat = document.querySelector(".controls__button--format");
+//   const btnMinify = document.querySelector(".controls__button--minify");
+
+//   btnFormat.addEventListener("click", () => {
+//   const formatted = JSON.stringify(JSON.parse(inputArea.value), null, 4);
+
+//   outputArea.value = formatted;
+// });
+
+// btnMinify.addEventListener("click", () => {
+//   const minified = JSON.stringify(JSON.parse(inputArea.value));
+
+//   outputArea.value = minified;
+// });
+
+//   return (
+//     <div className="App">
+//       <div class="container">
+//           <textarea class="large-area large-area--input" placeholder="Enter your JSON here..."></textarea>
+//         <div class="controls">
+//           <button type="button" class="controls__button controls__button--format">Format</button>
+//           <button type="button" class="controls__button controls__button--minify">Minify</button>
+//         </div>
+//         <textarea readonly class="large-area large-area--output" placeholder="Your JSON will appear here..."></textarea>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import "./App.css";
+import React, { useEffect, useState } from "react";
+export default function App() {
+  const [jsonString, setJsonString] = useState("");
+  const [prettierJson, setPrettierJson] = useState("");
+  
+  
+  
+//   useEffect(() => {
+//     console.log(jsonString);
+//     if(jsonString){
+//       const formatted = JSON.stringify(JSON.parse(jsonString), null, 4);
+//       setPrettierJson(formatted);
+//     }
+//     // const formatted = JSON.stringify(JSON.parse(jsonString), null, 4);
+// },[jsonString])
+const getPrettierJson = () => {
+  if(jsonString){
+    const formatted = JSON.stringify(JSON.parse(jsonString), null, 4);
+    setPrettierJson(formatted);
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <textarea type = "text" value = {jsonString} onChange = {(e) => {setJsonString(e.target.value)}}></textarea>
+     <button onClick={getPrettierJson} >Format</button>
+     <textarea type = "text" value = {prettierJson}></textarea>
     </div>
   );
 }
-
-export default App;
